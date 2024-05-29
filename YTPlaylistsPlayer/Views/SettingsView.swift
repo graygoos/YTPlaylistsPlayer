ba12edb804b9b2780d3cf3fd186df1bdfc9bbc01
Femi Aliu <femialiu713@gmail.com>
2024-05-29 13:08:18 +0100

    created SettingsScreen for signing out and other app settings

---
+++ YTPlaylistsPlayer/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  YTPlaylistsPlayer
+//
+//  Created by Femi Aliu on 29/05/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Form {
+            Text("Sign out")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
